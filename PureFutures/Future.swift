25dd394d26315e2c629d8ff43331522b58e39609
Victor Shamanov <wiruzx@gmail.com>
2015-02-11 03:18:55 +0300

    Add basic implemetation for Future<T, E>

---
+++ PureFutures/Future.swift
@@ -0,0 +1,94 @@
+//
+//  Future.swift
+//  PureFutures
+//
+//  Created by Виктор Шаманов on 2/11/15.
+//  Copyright (c) 2015 Victor Shamanov. All rights reserved.
+//
+
+import Foundation
+
+public class Future<T, E> {
+    
+    public typealias CompleteCallback = Result<T, E> -> Void
+    public typealias SuccessCallback = T -> Void
+    public typealias FailureCallback = E -> Void
+    
+    // MARK:- Private properties
+    
+    private let deferred = Deferred<Result<T, E>>()
+    
+    // MARK:- Public properties
+    
+    public var value: T? {
+        return deferred.value?.value
+    }
+    
+    // MARK:- Initialization
+    
+    private init(deferred: Deferred<Result<T, E>>) {
+        self.deferred = deferred
+    }
+    
+    // MARK:- Public methods
+    
+    public func onComplete(c: CompleteCallback) -> Future {
+        deferred.onComplete(c)
+        return self
+    }
+    
+    public func onSuccess(c: SuccessCallback) -> Future {
+        return onComplete {
+            switch $0 {
+            case .Success(let boxed):
+                c(boxed.value)
+            default:
+                break
+            }
+        }
+    }
+    
+    public func onFailure(c: FailureCallback) -> Future {
+        return onComplete {
+            switch $0 {
+            case .Error(let boxed):
+                c(boxed.value)
+            default:
+                break
+            }
+        }
+    }
+}
+
+public extension Future {
+    
+    public func map<U>(f: T -> U) -> Future<U, E> {
+        return flatMap { value  in
+            return Future<U, E>(deferred: Deferred.completed(.Success(Box(f(value)))))
+        }
+    }
+    
+    public func flatMap<U>(f: T -> Future<U, E>) -> Future<U, E> {
+        let p = Promise<Result<U, E>>()
+        
+        onComplete {
+            switch $0 {
+            case .Success(let boxed):
+                p.completeWith(f(boxed.value).deferred)
+            case .Error(let boxed):
+                p.complete(.Error(Box(boxed.value)))
+            }
+        }
+        
+        return Future<U, E>(deferred: p.deferred)
+    }
+    
+    public func mapResult<U>(f: T -> Result<U, E>) -> Future<U, E> {
+        return Future<U, E>(deferred: deferred.map { $0.flatMap(f) })
+    }
+    
+    public func filter(p: T -> Bool) -> Future<T?, E> {
+        return map { value in p(value) ? value : nil }
+    }
+    
+}
