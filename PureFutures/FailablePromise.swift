6adbad69fbada073a00a4322d0b4825c8f1a443c
Victor Shamanov <wiruzx@gmail.com>
2015-02-11 04:57:18 +0300

    Refactor FailablePromise

--- PureFutures/FailablePromise.swift
+++ PureFutures/FailablePromise.swift
@@ -10,26 +10,26 @@ import Foundation
 
 public final class FailablePromise<T, E> {
     
-    private var _future = Future<T, E>()
+    private let promise = Promise<Result<T, E>>()
     
     public var future: Future<T, E> {
-        return _future
+        return Future(deferred: promise.deferred)
     }
     
     public func complete(value: Result<T, E>) {
-        _future.deferred.result = value
+        promise.complete(value)
     }
     
     public func success(value: T) {
-        _future.deferred.result = .Success(Box(value))
+        complete(.Success(Box(value)))
     }
     
     public func failure(error: E) {
-        _future.deferred.result = .Error(Box(error))
+        complete(.Error(Box(error)))
     }
     
-    public func completeWith(future: Future<T, E>) {
-        _future = future
+    public func completeWith(value: Future<T, E>) {
+        promise.completeWith(value.deferred)
     }
     
 }
