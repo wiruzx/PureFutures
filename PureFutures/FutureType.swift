036c6dce276126dd8985359d0bb75d2825a083eb
Victor Shamanov <wiruzx@gmail.com>
2015-02-28 18:30:14 +0300

    Fix name

--- PureFutures/FutureType.swift
+++ PureFutures/FutureType.swift
@@ -2,7 +2,7 @@
 //  FutureType.swift
 //  PureFutures
 //
-//  Created by Виктор Шаманов on 2/11/15.
+//  Created by Victor Shamanov on 2/11/15.
 //  Copyright (c) 2015 Victor Shamanov. All rights reserved.
 //
 
--- PureFutures/Result.swift
+++ PureFutures/Result.swift
@@ -2,7 +2,7 @@
 //  Result.swift
 //  PureFutures
 //
-//  Created by Виктор Шаманов on 2/10/15.
+//  Created by Victor Shamanov on 2/10/15.
 //  Copyright (c) 2015 Victor Shamanov. All rights reserved.
 //
 
