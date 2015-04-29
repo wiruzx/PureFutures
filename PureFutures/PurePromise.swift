c0d7d7ae5c2c607a58e1fdaa172caf62a1096aa2
Victor Shamanov <wiruzx@gmail.com>
2015-04-29 23:02:53 +0300

    Add documentation for PurePromise

--- PureFutures/PurePromise.swift
+++ PureFutures/PurePromise.swift
@@ -6,12 +6,21 @@
 //  Copyright (c) 2015 Victor Shamanov. All rights reserved.
 //
 
+/**
+
+    A mutable container for the `Deferred`
+
+    Allows you to complete `Deferred` that it holds
+
+*/
 public struct PurePromise<T> {
     
     // MARK:- Public properties
     
+    /// `Deferred` which PurePromise will complete
     public let deferred = Deferred<T>()
     
+    /// Shows if its deferred is completed
     public var isCompleted: Bool {
         return deferred.isCompleted
     }
@@ -23,16 +32,51 @@ public struct PurePromise<T> {
     
     // MARK:- PurePromiseType methods
     
+    /**
+    
+        Completes PurePromise's deferred with given `value`
+    
+        Should be called only once! Second and next calls will raise an exception
+    
+        See also: `tryComplete`
+    
+        :param: value value that deferred will be completed with
+    
+    */
     public func complete(value: T) {
         deferred.setValue(value)
     }
     
+    /**
+    
+        Completes PurePromise's deferred with given deferred
+    
+        Should be called only once! Second and next calls will raise an exception
+    
+        See also: `tryCompleteWith`
+    
+        :param: deferred Value that conforms to `DeferredType` protocol
+    
+    */
     public func completeWith<D: DeferredType where D.Element == T>(deferred: D) {
         deferred.onComplete(ExecutionContext.DefaultPureOperationContext) { self.complete($0) }
     }
     
     // MARK:- Other methods
     
+    /**
+    
+        Tries to complete PurePromise's deferred with given `value`.
+    
+        If deferred was already completed returns `false`, otherwise returns `true`
+    
+        See also: `complete`
+    
+        :param: value value that deferred will be completed with
+    
+        :returns: Bool which says if completing was successful or not
+
+    */
     public func tryComplete(value: T) -> Bool {
         if isCompleted {
             return false
@@ -42,6 +86,19 @@ public struct PurePromise<T> {
         }
     }
     
+    /**
+    
+        Tries to complete PurePromise's deferred with given deferred
+    
+        If deferred has already completed returns `false`, otherwise returns `true`
+    
+        See also: `completeWith`
+    
+        :param: deferred Value that should conform to `DeferredType` protocol
+    
+        :returns: Bool which says if completing was successful or not
+
+    */
     public func tryCompleteWith<D: DeferredType where D.Element == T>(deferred: D) {
         deferred.onComplete(ExecutionContext.DefaultPureOperationContext) { self.tryComplete($0) }
     }
