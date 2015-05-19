fcd1586449a97382c65c98011727a846e3df266b
Victor Shamanov <wiruzx@gmail.com>
2015-05-20 01:02:09 +0300

    Add default execution context for Deferred and Future type functions

--- PureFutures/FutureTypeFunctions.swift
+++ PureFutures/FutureTypeFunctions.swift
@@ -44,14 +44,14 @@ public func ??<F: FutureType>(fx: F, x: F) -> Future<F.SuccessType, F.ErrorType>
     and returns a new future with the result of this future
 
     :param: f side-effecting function that will be applied to result of `fx`
-    :param: ec execution context of `f` function
+    :param: ec execution context of `f` function. By default is main queue
 
     :param: fx Future
 
     :returns: a new Future
 
 */
-public func andThen<F: FutureType>(f: F.SuccessType -> Void, ec: ExecutionContextType)(_ fx: F) -> Future<F.SuccessType, F.ErrorType> {
+public func andThen<F: FutureType>(f: F.SuccessType -> Void, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext)(_ fx: F) -> Future<F.SuccessType, F.ErrorType> {
     let p = Promise<F.SuccessType, F.ErrorType>()
     fx.onComplete(ec) {
         switch $0 {
@@ -70,14 +70,14 @@ public func andThen<F: FutureType>(f: F.SuccessType -> Void, ec: ExecutionContex
     Creates a new future by applying a function `f` to the success result of this future.
 
     :param: f Function that will be applied to success result of `fx`
-    :param: ec Execution context of `f`
+    :param: ec Execution context of `f`. By default is global queue
 
     :param: Future
 
     :returns: a new Future
 
 */
-public func map<F: FutureType, T>(f: F.SuccessType -> T, ec: ExecutionContextType) -> F -> Future<T, F.ErrorType> {
+public func map<F: FutureType, T>(f: F.SuccessType -> T, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext) -> F -> Future<T, F.ErrorType> {
     return transform(f, id, ec)
 }
 
@@ -87,13 +87,13 @@ public func map<F: FutureType, T>(f: F.SuccessType -> T, ec: ExecutionContextTyp
 
     :param: s Function that will be applied to success result of `fx`
     :param: e Function that will be applied to failed result of `fx`
-    :param: ec Execution context of `s` and `e` functions
+    :param: ec Execution context of `s` and `e` functions. By default is global queue
 
     :param: fx Future
 
     :returns: a new Future
 */
-public func transform<F: FutureType, S, E>(s: F.SuccessType -> S, e: F.ErrorType -> E, ec: ExecutionContextType)(_ fx: F) -> Future<S, E> {
+public func transform<F: FutureType, S, E>(s: F.SuccessType -> S, e: F.ErrorType -> E, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext)(_ fx: F) -> Future<S, E> {
     let p = Promise<S, E>()
     fx.onComplete(ec) {
         switch $0 {
@@ -111,14 +111,14 @@ public func transform<F: FutureType, S, E>(s: F.SuccessType -> S, e: F.ErrorType
     Creates a new future by applying a function to the success result of this future, and returns the result of the function as the new future.
 
     :param: f Funcion that will be applied to success result of `fx`
-    :param: ec Execution context of `f`
+    :param: ec Execution context of `f`. By default is global queue
 
     :param: fx Future
 
     :returns: a new Future
 
 */
-public func flatMap<F: FutureType, F2: FutureType where F.ErrorType == F2.ErrorType>(f: F.SuccessType -> F2, ec: ExecutionContextType)(_ fx: F) -> Future<F2.SuccessType, F2.ErrorType> {
+public func flatMap<F: FutureType, F2: FutureType where F.ErrorType == F2.ErrorType>(f: F.SuccessType -> F2, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext)(_ fx: F) -> Future<F2.SuccessType, F2.ErrorType> {
     let p = Promise<F2.SuccessType, F2.ErrorType>()
     fx.onComplete(ec) {
         switch $0 {
@@ -164,14 +164,14 @@ public func flatten<F: FutureType, IF: FutureType where F.SuccessType == IF, F.E
     Creates a new Future by filtering the value of the current Future with a predicate `p`
 
     :param: p Predicate function
-    :param: ec Execution context of `p`
+    :param: ec Execution context of `p`. By default is global queue
 
     :param: Future
 
     :returns: A new Future with value or nil
 
 */
-public func filter<F: FutureType>(p: F.SuccessType -> Bool, ec: ExecutionContextType) -> F -> Future<F.SuccessType?, F.ErrorType> {
+public func filter<F: FutureType>(p: F.SuccessType -> Bool, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext) -> F -> Future<F.SuccessType?, F.ErrorType> {
     return map({ x in p(x) ? x : nil }, ec)
 }
 
@@ -202,14 +202,14 @@ public func zip<F: FutureType, T: FutureType where F.ErrorType == T.ErrorType>(f
 
     :param: initial Initial value that will be passed as first argument in `combine` function
     :param: combine reducing function
-    :param: ec Execution context of `combine`
+    :param: ec Execution context of `combine`. By default is global queue
 
     :param: fxs Sequence of Futures
 
     :returns: Future which will contain result of reducing sequence of futures
 
 */
-public func reduce<S: SequenceType, T where S.Generator.Element: FutureType>(initial: T, combine: (T, S.Generator.Element.SuccessType) -> T, ec: ExecutionContextType)(_ fxs: S) -> Future<T, S.Generator.Element.ErrorType> {
+public func reduce<S: SequenceType, T where S.Generator.Element: FutureType>(initial: T, combine: (T, S.Generator.Element.SuccessType) -> T, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext)(_ fxs: S) -> Future<T, S.Generator.Element.ErrorType> {
     return reduce(fxs, Future(.success(initial))) { acc, futureValue in
         flatMap({ value in
             map({ combine($0, value) }, ec)(acc)
@@ -222,14 +222,14 @@ public func reduce<S: SequenceType, T where S.Generator.Element: FutureType>(ini
     Transforms a sequence of values into Future of array of this values using the provided function `f`
 
     :param: f Function for transformation values into Future
-    :param: ec Execution context of `f`
+    :param: ec Execution context of `f`. By default is global queue
 
     :param: xs Sequence of values
 
     :returns: a new Future
 
 */
-public func traverse<S: SequenceType, F: FutureType>(f: S.Generator.Element -> F, ec: ExecutionContextType)(_ xs: S) -> Future<[F.SuccessType], F.ErrorType> {
+public func traverse<S: SequenceType, F: FutureType>(f: S.Generator.Element -> F, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext)(_ xs: S) -> Future<[F.SuccessType], F.ErrorType> {
     return reduce([], { $0 + [$1] }, ec)(map(xs, f))
 }
 
@@ -256,14 +256,14 @@ public func sequence<S: SequenceType where S.Generator.Element: FutureType>(fxs:
     See: `toDeferred`
 
     :param: r Recover function
-    :param: ec Execution context of `r` function
+    :param: ec Execution context of `r` function. By default is global queue
 
     :param: fx Future
 
     :returns: a new Future that will never fail
 
 */
-public func recover<F: FutureType>(r: F.ErrorType -> F.SuccessType, ec: ExecutionContextType)(_ fx: F) -> Future<F.SuccessType, F.ErrorType> {
+public func recover<F: FutureType>(r: F.ErrorType -> F.SuccessType, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext)(_ fx: F) -> Future<F.SuccessType, F.ErrorType> {
     let p = Promise<F.SuccessType, F.ErrorType>()
     fx.onComplete(ec) {
         switch $0 {
@@ -281,14 +281,14 @@ public func recover<F: FutureType>(r: F.ErrorType -> F.SuccessType, ec: Executio
     Creates a new future that will handle fail results that this future might contain by assigning it a value of another future.
 
     :param: r Recover function
-    :param: ec Execition context of `r` function
+    :param: ec Execition context of `r` function. By default is global queue
 
     :param: fx Future
 
     :returns: a new Future
 
 */
-public func recoverWith<F: FutureType>(r: F.ErrorType -> F, ec: ExecutionContextType)(_ fx: F) -> Future<F.SuccessType, F.ErrorType> {
+public func recoverWith<F: FutureType>(r: F.ErrorType -> F, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext)(_ fx: F) -> Future<F.SuccessType, F.ErrorType> {
     let p = Promise<F.SuccessType, F.ErrorType>()
     fx.onComplete(ec) {
         switch $0 {
@@ -323,14 +323,14 @@ public func toDeferred<F: FutureType>(fx: F) -> Deferred<Result<F.SuccessType, F
     Transforms Future<T, E> into Deferred<T> and handles error case with `r` function
 
     :param: r Recover function
-    :param: ec Execution context of `r` function
+    :param: ec Execution context of `r` function. By default is global queue
 
     :param: fx Future
 
     :returns: Deferred with success value of `fx` or result of `r`
 
 */
-public func toDeferred<F: FutureType>(r: F.ErrorType -> F.SuccessType, ec: ExecutionContextType)(_ fx: F) -> Deferred<F.SuccessType> {
+public func toDeferred<F: FutureType>(r: F.ErrorType -> F.SuccessType, _ ec: ExecutionContextType = ExecutionContext.DefaultPureOperationContext)(_ fx: F) -> Deferred<F.SuccessType> {
     let p = PurePromise<F.SuccessType>()
     fx.onComplete(ec) {
         switch $0 {
