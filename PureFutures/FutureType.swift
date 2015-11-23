a4b0f1f5114ee7fe01c4314cad1f61a23347d215
Victor Shamanov <wiruzx@gmail.com>
2015-11-24 01:00:37 +0300

    Remove unused import

--- PureFutures/FutureType.swift
+++ PureFutures/FutureType.swift
@@ -5,7 +5,7 @@
 //  Created by Victor Shamanov on 2/11/15.
 //  Copyright (c) 2015 Victor Shamanov. All rights reserved.
 //
-import enum Result.Result
+
 import protocol Result.ResultType
 
 public protocol FutureType: DeferredType {
